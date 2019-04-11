e34a3c4065b78f3690cb44e2c373c6ff9295b4ce
alber <alber@IndwarCorn060>
2019-04-11 22:59:26 +0200

    las flechas y el intro fincionan, no cuando esta en el boton

--- SudokuGit/src/sudokuGit/SudokuVentana.java
+++ SudokuGit/src/sudokuGit/SudokuVentana.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
@@ -13,10 +14,11 @@ public class SudokuVentana extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField[][] input;
+	private JTextField[] input;
 	private JButton resolver;
 	private JButton reset;
 	private JCheckBoxMenuItem mLog;
+	private JCheckBoxMenuItem mDesplazar;
 	private JTextArea txt;
 	private JScrollPane log;
 	private Container cp;
@@ -25,14 +27,18 @@ public class SudokuVentana extends JFrame{
 	
 	public SudokuVentana(String titulo) {
 		super(titulo);
-		this.input = new JTextField[9][9];
+		this.input = new JTextField[81];
 		this.resolver = new JButton("Resolver");
 		this.reset = new JButton("Resetear");
 		this.mLog = new JCheckBoxMenuItem("Mostrar Log");
 		this.mLog.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+		this.mDesplazar = new JCheckBoxMenuItem("Desplazar al escribir");
+		this.mDesplazar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F2, 0));
+		this.mDesplazar.setSelected(true);
 		this.sudokuPrueba = new JMenuItem("Cargar sudoku prueba");
 		this.sudokuAleatorio = new JMenuItem("Sudoku Aleatorio");
 		this.txt = new JTextArea();
+		this.txt.setEditable(false);
 		this.log = new JScrollPane(txt);
 		
 		JMenuBar barra = new JMenuBar();
@@ -41,6 +47,7 @@ public class SudokuVentana extends JFrame{
 			mSudoku.add(this.sudokuAleatorio);
 			JMenu mVer = new JMenu("Ver");
 			mVer.add(this.mLog);
+			mVer.add(this.mDesplazar);
 		barra.add(mSudoku);
 		barra.add(mVer);
 		this.setJMenuBar(barra);
@@ -49,10 +56,8 @@ public class SudokuVentana extends JFrame{
 		
 		JPanel centro = new JPanel(new GridLayout(9,9));
 			for(int i=0; i<this.input.length; i++) {
-				for(int e=0; e<this.input[i].length; e++) {
-					this.input[i][e] = new JTextField(1);
-					centro.add(this.input[i][e]);
-				}
+				this.input[i] = new JTextField(1);
+				centro.add(this.input[i]);
 			}
 		centro.setPreferredSize(new Dimension(180,180));
 		JPanel sur = new JPanel();
@@ -70,6 +75,9 @@ public class SudokuVentana extends JFrame{
 		this.mLog.addActionListener(cb);
 		this.sudokuPrueba.addActionListener(cb);
 		this.sudokuAleatorio.addActionListener(cb);
+		for(int i=0; i<this.input.length; i++) {
+			this.input[i].addKeyListener(cb);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -85,7 +93,7 @@ public class SudokuVentana extends JFrame{
 
 	}
 	
-	class ControlBoton implements ActionListener{
+	class ControlBoton implements ActionListener, KeyListener{
 		
 		private JFrame ventana;
 		
@@ -97,28 +105,25 @@ public class SudokuVentana extends JFrame{
 			MetodosSudokuV2Objeto sudoku = new MetodosSudokuV2Objeto();
 			int cont=0;
 			for(int i=0; i<input.length; i++) {
-				for(int e=0; e<input[i].length; e++) {
-					try {
-						sudoku.insertarElemento(i+1, e+1, Integer.parseInt(input[i][e].getText()));
-					}
-					catch(Exception ex) {
-						if(!input[i][e].getText().equals("")) {
-							cont++;
-							input[i][e].setText("");
-						}
+				 try {
+					sudoku.insertarElemento(i/9+1, i%9+1, Integer.parseInt(input[i].getText()));
+				}
+				catch(Exception ex) {
+					if(!input[i].getText().equals("")) {
+						cont++;
+							input[i].setText("");
 					}
 				}
+				
 			}
 			if(cont>0){
 				JOptionPane.showMessageDialog(null, "Datos mal introducidos", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 			sudoku.resolverSudoku();
 			for(int i=0; i<input.length; i++) {
-				for(int e=0; e<input[i].length; e++) {
-					if(input[i][e].getText().equals("")) {
-						input[i][e].setText(String.valueOf(sudoku.getValor(i, e)));
-						input[i][e].setBackground(Color.LIGHT_GRAY);
-					}
+				if(input[i].getText().equals("")) {
+					input[i].setText(String.valueOf(sudoku.getValor(i/9, i%9)));
+					input[i].setBackground(Color.LIGHT_GRAY);
 				}
 			}
 			txt.append(sudoku.getTxt());
@@ -128,10 +133,8 @@ public class SudokuVentana extends JFrame{
 		
 		private void metodoReset() {
 			for(int i=0; i<input.length; i++) {
-				for(int e=0; e<input[i].length; e++) {
-					input[i][e].setText(null);	
-					input[i][e].setBackground(Color.WHITE);
-				}
+				input[i].setText(null);	
+				input[i].setBackground(Color.WHITE);
 			}
 			txt.setText(null);
 			resolver.setEnabled(true);
@@ -139,31 +142,31 @@ public class SudokuVentana extends JFrame{
 		
 		public void metodoSudokuPrueba() {
 			metodoReset();
-			input[0][0].setText("2");
-			input[1][2].setText("1");
-			input[1][3].setText("7");
-			input[1][4].setText("8");
-			input[1][8].setText("2");
-			input[2][1].setText("6");
-			input[2][2].setText("7");
-			input[2][3].setText("4");
-			input[3][1].setText("2");
-			input[3][3].setText("3");
-			input[3][6].setText("9");
-			input[4][1].setText("4");
-			input[4][5].setText("7");
-			input[4][6].setText("2");
-			input[5][2].setText("3");
-			input[6][4].setText("9");
-			input[6][6].setText("4");
-			input[6][7].setText("8");
-			input[7][0].setText("9");
-			input[7][2].setText("2");
-			input[7][4].setText("4");
-			input[7][6].setText("6");
-			input[7][7].setText("7");
-			input[8][0].setText("6");
-			input[8][3].setText("8");
+			input[0].setText("2");
+			input[11].setText("1");
+			input[12].setText("7");
+			input[13].setText("8");
+			input[17].setText("2");
+			input[19].setText("6");
+			input[20].setText("7");
+			input[21].setText("4");
+			input[28].setText("2");
+			input[30].setText("3");
+			input[33].setText("9");
+			input[37].setText("4");
+			input[41].setText("7");
+			input[32].setText("2");
+			input[47].setText("3");
+			input[58].setText("9");
+			input[60].setText("4");
+			input[61].setText("8");
+			input[63].setText("9");
+			input[65].setText("2");
+			input[67].setText("4");
+			input[69].setText("6");
+			input[70].setText("7");
+			input[72].setText("6");
+			input[75].setText("8");
 		}
 		
 		public void sudokuAleatorio() {
@@ -172,12 +175,10 @@ public class SudokuVentana extends JFrame{
 			System.out.println(cod);
 			int cont=0;
 			for(int i=0; i<input.length; i++) {
-				for(int e=0; e<input[i].length; e++) {
-					if(cod.charAt(cont)!='.') {
-						input[i][e].setText(Character.toString(cod.charAt(cont)));
-					}
-					cont++;
+				if(cod.charAt(cont)!='.') {
+					input[i].setText(Character.toString(cod.charAt(cont)));
 				}
+				cont++;
 			}
 		}
 		
@@ -218,6 +219,87 @@ public class SudokuVentana extends JFrame{
 			}
 			if(evento.getSource()==sudokuAleatorio) {
 				this.confirmacionAleatorio();
+			}
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent evento) {
+			// TODO Auto-generated method stub
+			if(evento.getSource() instanceof JTextField) {
+				switch(evento.getKeyCode()) {
+					case KeyEvent.VK_DOWN:
+						for(int i=0; i<input.length; i++) {
+							if(input[i].equals(evento.getSource())) {
+								input[(i+9)%81].grabFocus();
+								i=81;
+							}
+						}
+						break;
+					case KeyEvent.VK_UP:
+						for(int i=0; i<input.length; i++) {
+							if(input[i].equals(evento.getSource())) {
+								input[(i+72)%81].grabFocus();
+								i=81;
+							}
+						}
+						break;
+					case KeyEvent.VK_LEFT:
+						for(int i=0; i<input.length; i++) {
+							if(input[i].equals(evento.getSource())) {
+								input[((i+80)%9)+i/9*9].grabFocus();
+								i=81;
+							}
+						}
+						break;
+					case KeyEvent.VK_ENTER:
+						for(int i=0; i<input.length; i++) {
+							if(input[i].equals(evento.getSource())) {
+								try {
+									input[(i+1)].grabFocus();
+								}catch(Exception e) {
+									resolver.grabFocus();
+								}
+							}
+						}
+						break;
+					case KeyEvent.VK_RIGHT:
+						for(int i=0; i<input.length; i++) {
+							if(input[i].equals(evento.getSource())) {
+								input[((i+1)%9)+i/9*9].grabFocus();
+								i=81;
+							}
+						}
+						break;
+					default:
+						break;
+				}
+			}
+			if(evento.getKeyCode()==KeyEvent.VK_ENTER&&resolver.isFocusOwner()) {
+				resolver.doClick();
+				System.out.println("ye");
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent evento) {
+			// TODO Auto-generated method stub
+			if((evento.getSource() instanceof JTextField)&&mDesplazar.isSelected()) {
+				for(int i=0; i<input.length; i++) {
+					if(input[i].equals(evento.getSource())) {
+						try {
+							input[(i+1)].grabFocus();
+						}catch(Exception e) {
+							resolver.grabFocus();
+						}
+					}
+				}
 			}
 		}
 	}
